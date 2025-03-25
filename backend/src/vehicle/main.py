4798e1ee2af882d6b183ae68857cce2bcd6df08c
Алина <fransili2702@gmail.com>
2025-03-25 19:21:47 +0300

    initial alembic and changed compose for development

--- backend/src/vehicle/main.py
+++ backend/src/vehicle/main.py
@@ -15,4 +15,4 @@ for router in all_routers:
     app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
